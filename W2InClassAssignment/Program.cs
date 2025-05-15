2470ccdeaae6530b7bfb9febe2dc19d406d66e4c
Berk <alan0053@algonquinlive.com>
2025-05-15 18:41:57 -0400

    added Point func

--- W2InClassAssignment/Program.cs
+++ W2InClassAssignment/Program.cs
@@ -8,5 +8,18 @@ class Program
         var str = p.ToString();
         Console.WriteLine(str);
     }
-    
+
+    class Point
+    {
+        public int X;
+        public int Y;
+
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
 }
