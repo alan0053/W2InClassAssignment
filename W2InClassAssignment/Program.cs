562a5b4149e54b3b42fe353f0f68a18014280114
Berk <alan0053@algonquinlive.com>
2025-05-15 18:32:35 -0400

    init

---
+++ W2InClassAssignment/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace W2InClassAssignment;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Point p = new Point(1, 2);
+        var str = p.ToString();
+        Console.WriteLine(str);
+    }
+    
+}
